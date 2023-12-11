99ec4be4da6ae553a7728e919feb4c2ffa79e14e
KleinNotana <21120303@student.hcmus.edu.vn>
2023-12-11 15:13:38 +0700

    update Bussiness Prototype

---
+++ BUS_QLSTK/Business.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BUS_QLSTK
+{
+    public class Business
+    {
+        bool create_SoTietKiem(string maSo, string CCCD, string tenKH, string diaChi, int kyHan, long soTien)
+        {
+            return true;
+        }
+        
+
+        bool create_PhieuGui(string maSo, string CCCD, long soTien)
+        {
+            return true;
+        }
+
+        bool create_PhieuRut(string maSo, string CCCD, long soTien)
+        {
+            return true;
+        }
+
+        int getNew_MaSo()
+        {
+            return 1;
+        }
+
+        List<dynamic> getList_SoTietKiem()
+        {
+            return new List<dynamic>();
+        }
+
+        //todo: đồng nhất kiểu Date
+        List<dynamic> getList_DoanhSoNgay(Date ngay)
+        {
+            return new List<dynamic>();
+        }
+
+        List<dynamic> getList_BaoCaoDongMoSoThang(int thang, int nam)
+        {
+            return new List<dynamic>();
+        }
+
+        //todo: sửa kiểu dữ liệu dynamic thành LoaiTietKiem
+        List<dynamic> getList_LoaiTietKiem()
+        {
+            return new List<dynamic>();
+        }
+
+        bool add_LoaiTietKiem(int kyHan, double laiSuat)
+        {
+            return true;
+        }
+
+        //todo sửa kiểu dữ liệu dynamic thành LoaiTietKiem
+        bool delete_LoaiTietKiem(dynamic loaiTietKiem)
+        {
+            return true;
+        }
+
+        bool update_NgayGuiToiThieu(int ngay)
+        {
+            return true;
+        }
+
+        bool update_SoTienGuiToiThieu(int ngay)
+        {
+            return true;
+        }
+
+        int get_NgayGuiToiThieu()
+        {
+            return 1;
+        }   
+
+        int get_SoTienGuiToiThieu()
+        {
+            return 1;
+        }
+
+    }
+
+}
