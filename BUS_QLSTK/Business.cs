2628320b775e82bd5bc76d28213993db8572e0e4
KleinNotana <21120303@student.hcmus.edu.vn>
2023-12-12 15:51:44 +0700

    update bus

--- BUS_QLSTK/Business.cs
+++ BUS_QLSTK/Business.cs
@@ -7,6 +7,7 @@ using System.Reflection.Metadata;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using DAL_QLSTK;
 using DTO_QLSTK;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace BUS_QLSTK
 {
@@ -40,10 +41,10 @@ namespace BUS_QLSTK
                            
             }
         }
-        bool create_SoTietKiem( string CCCD, string tenKH, string diaChi, int kyHan, long soTien)
+        public bool create_SoTietKiem(int maSo, string CCCD, string tenKH, string diaChi, int kyHan, long soTien)
         {
             var soTietKiem = new SoTietKiem();
-            soTietKiem.Maso = getNew_MaSo();
+            soTietKiem.Maso = maSo;
             soTietKiem.Cccd = CCCD;
             var currentDateTime = DateTime.Now;
             soTietKiem.Ngaymoso = currentDateTime;
@@ -96,15 +97,21 @@ namespace BUS_QLSTK
 
             if (result)
             {
+                var phieuGui = new PhieuGui();
+                phieuGui.Maso = soTietKiem.Maso;
+                phieuGui.Ngaygui = DateTime.Now;
+                phieuGui.Sotien = soTien;
+                phieuGui.Maphieugui = getNew_MaPhieuGui();
+                PhieuGui.Add_PhieuGui(phieuGui);
                 KhachHang.Add_KhachHang(kh);
             }
               
 
             return result;  
         }
-        
 
-        bool create_PhieuGui(int maSo, string CCCD, long soTien)
+
+        public bool create_PhieuGui(int maSo, string CCCD, long soTien)
         {
             var result = true;
 
@@ -149,7 +156,7 @@ namespace BUS_QLSTK
                 phieuGui.Maso = maSo;
                 phieuGui.Ngaygui = DateTime.Now;
                 phieuGui.Sotien = soTien;
-                phieuGui.Maphieugui = getNew_MaSo();
+                phieuGui.Maphieugui = getNew_MaPhieuGui();
 
                 PhieuGui.Add_PhieuGui(phieuGui);
             }
@@ -157,15 +164,128 @@ namespace BUS_QLSTK
             return result;
         }
 
-        bool create_PhieuRut(int maSo, string CCCD, long? soTien)
+        public bool create_PhieuRut(int maSo, string CCCD, long? soTien)
         {
+            var result = true;
 
-            return true;
+            var listSoTietKiem = SoTietKiem.GetList_SoTietKiem();
+
+            SoTietKiem? soTietKiem = null;
+
+            foreach(var soTK in listSoTietKiem)
+            {
+                if(soTK.Maso == maSo)
+                {
+
+                    soTietKiem = soTK;
+                    
+                    break;
+                }
+            }
+
+
+            if (soTietKiem.Trangthai == 0)
+            {
+                result = false;
+                throw new Exception("Sổ đã đóng");
+            }
+
+            else if (soTietKiem.Loaitietkiem == 0)
+            {
+                var soNgayGui = (DateTime.Now - soTietKiem.Ngaymoso)?.Days;
+
+                if(soNgayGui < soTietKiem.Songayduocrut)
+                {
+                    result = false;
+                    throw new Exception("Sổ chưa đủ ngày gửi");
+                }
+                else
+                {
+                    var listPhieuRut = PhieuRut.GetList_PhieuRut();
+
+                    DateTime? latestDate = DateTime.MinValue;
+
+                    foreach (var phieuRut in listPhieuRut)
+                    {
+                        if (phieuRut.Maso == maSo)
+                        {
+                            latestDate = phieuRut?.Ngayrut > latestDate ? phieuRut.Ngayrut : latestDate;
+                        }
+                    }
+
+                    var soThangGui = (DateTime.Now - latestDate)?.Days / 30;
+
+                    if (latestDate != DateTime.MinValue)
+                    {
+                        var soThangDaTinhLai = (latestDate - soTietKiem.Ngaymoso)?.Days / 30;
+                        
+                        soThangGui -= soThangDaTinhLai;
+                    }
+
+                    soTietKiem.Sodu += (long)(soTietKiem.Sodu * soTietKiem.Laisuat * soThangGui );
+
+                    if(soTien > soTietKiem.Sodu)
+                    {
+                        result = false;
+                        throw new Exception("Số tiền rút lớn hơn số dư");
+                    }
+                    else
+                    {
+                        soTietKiem.Sodu -= soTien;
+                        if(soTietKiem.Sodu == 0)
+                        {
+                            soTietKiem.Trangthai = 0;
+                            soTietKiem.Ngaydongso = DateTime.Now;
+                        }
+                        SoTietKiem.Update_SoTietKiem(soTietKiem);
+
+                        var phieuRut = new PhieuRut();
+                        phieuRut.Maso = maSo;
+                        phieuRut.Ngayrut = DateTime.Now;
+                        phieuRut.Sotien = soTien;
+                        phieuRut.Maphieurut = getNew_MaPhieuRut();
+                        PhieuRut.Add_PhieuRut(phieuRut);
+
+                    }
+                }
+            }
+
+            else
+            {
+                var soNgayGui = (DateTime.Now - soTietKiem.Ngaymoso)?.Days;
+                var soThangGui = soNgayGui / 30;
+
+                if(soThangGui < soTietKiem.Loaitietkiem)
+                {
+                    result = false;
+                    throw new Exception("Sổ chưa đủ kỳ hạn");
+                }
+                else
+                {
+                    var soLanDaoHan = soThangGui / soTietKiem.Loaitietkiem;
+                   soTietKiem.Sodu += (long)(soTietKiem.Sodu * soTietKiem.Laisuat * soLanDaoHan * soTietKiem.Loaitietkiem);
+                    soTien = soTietKiem.Sodu;
+                    soTietKiem.Sodu = 0;
+                    soTietKiem.Trangthai = 0;
+                    soTietKiem.Ngaydongso = DateTime.Now;
+                    SoTietKiem.Update_SoTietKiem(soTietKiem);
+
+                    var phieuRut = new PhieuRut();
+                    phieuRut.Maso = maSo;
+                    phieuRut.Ngayrut = DateTime.Now;
+                    phieuRut.Sotien = soTien;
+                    phieuRut.Maphieurut = getNew_MaPhieuRut();
+                    PhieuRut.Add_PhieuRut(phieuRut);
+                    
+                }
+            }
+
+            return result;
 
             
         }
 
-        int getNew_MaSo()
+        public int getNew_MaSo()
         {
             DAL_SoTietKiem dal = DAL_SoTietKiem.Instance;
             var list_SoTietKiem = dal.GetList_SoTietKiem();
@@ -174,56 +294,120 @@ namespace BUS_QLSTK
             return res;
         }
 
-        List<dynamic> getList_SoTietKiem()
+        public int getNew_MaPhieuGui()
+        {
+            var list_PhieuGui = PhieuGui.GetList_PhieuGui();
+            var max = list_PhieuGui.Max(x => x.Maphieugui);
+            var res = max + 1;
+            return res;
+        }
+
+
+        public int getNew_MaPhieuRut()
+        {
+            var list_PhieuRut = PhieuRut.GetList_PhieuRut();
+            var max = list_PhieuRut.Max(x => x.Maphieurut);
+            var res = max + 1;
+            return res;
+        }
+
+        public List<dynamic> getList_SoTietKiem()
         {
             return new List<dynamic>();
         }
 
         //todo: đồng nhất kiểu Date
-        List<dynamic> getList_DoanhSoNgay(Date ngay)
+        public List<dynamic> getList_DoanhSoNgay(Date ngay)
         {
             return new List<dynamic>();
         }
 
-        List<dynamic> getList_BaoCaoDongMoSoThang(int thang, int nam)
+        public List<dynamic> getList_BaoCaoDongMoSoThang(int thang, int nam)
         {
             return new List<dynamic>();
         }
 
-        List<LoaiTietKiem> getList_LoaiTietKiem()
+        public List<LoaiTietKiem> getList_LoaiTietKiem()
         {
-            return new List<LoaiTietKiem>();
+            return Config.GetList_LoaiTietKiem();
         }
 
-        bool add_LoaiTietKiem(int kyHan, double laiSuat)
+        public bool add_LoaiTietKiem(int kyHan, double laiSuat)
         {
-            return true;
+            var result = true;
+
+            var listLoaiTietKiem = Config.GetList_LoaiTietKiem();
+
+            foreach(var loaiTietKiem in listLoaiTietKiem)
+            {
+                if(loaiTietKiem.Kyhan == kyHan)
+                {
+                    result = false;
+                    throw new Exception("Loại tiết kiệm đã tồn tại");
+                }
+            }
+
+            if(result)
+            {
+                var loaiTietKiem = new LoaiTietKiem();
+                loaiTietKiem.Kyhan = kyHan;
+                loaiTietKiem.Laisuat = laiSuat;
+                Config.Add_LoaiTietKiem(loaiTietKiem);
+            }
+
+            return result;
         }
 
-       
-        bool delete_LoaiTietKiem(LoaiTietKiem loaiTietKiem)
+
+        public bool delete_LoaiTietKiem(LoaiTietKiem loaiTietKiem)
         {
-            return true;
+            var result = true;
+            
+            Config.Delete_LoaiTietKiem(loaiTietKiem);
+
+            return result;
         }
 
-        bool update_NgayGuiToiThieu(int ngay)
+        public bool update_NgayGuiToiThieu(int ngay)
         {
-            return true;
+            var result = true;
+            if (ngay > 0)
+            {
+                   Config.Update_SoNgayGuiToiThieu(ngay);
+            }
+            else
+            {
+                result = false;
+                throw new Exception("Ngày gửi tối thiểu không hợp lệ");
+            }
+            return result;
         }
 
-        bool update_SoTienGuiToiThieu(int ngay)
+        public bool update_SoTienGuiToiThieu(int soTien)
         {
-            return true;
+            var result = true;
+
+            if(soTien <= 0)
+            {
+                result = false;
+                throw new Exception("Số tiền gửi tối thiểu không hợp lệ");
+            }
+            else
+            {
+                Config.Update_SoTienGuiToiThieu(soTien);
+            }
+
+            return result;
         }
 
-        int get_NgayGuiToiThieu()
+        public int get_NgayGuiToiThieu()
         {
-            return 1;
-        }   
+            return Config.Get_SoNgayGuiToiThieu();
+        }
 
-        int get_SoTienGuiToiThieu()
+        public long get_SoTienGuiToiThieu()
         {
-            return 1;
+            return Config.Get_SoTienGuiToiThieu();
         }
 
     }
